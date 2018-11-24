a73d9d7eff878504baff2279e484958d4b16d853
manemone <hamada.kazuki.mum@gmail.com>
2018-11-24 19:08:43 +0900

    Split Dockerfile into two: for CPU / GPU envirnment

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker build $@ -t manemonetech/ml-base .
+docker build $@ -f Dockerfile.gpu -t manemonetech/ml-base .
+docker build $@ -f Dockerfile.cpu -t manemonetech/ml-base-cpu .
